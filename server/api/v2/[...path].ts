0c5e5a64295bb4d16ddf50a4a3a475d4c39d55f9
Ian Febi Sastrataruna <IAN463@PID-PROJECT-IAN643.local>
2023-08-03 15:27:04 +0700

    huaa

--- server/api/v2/[...path].ts
+++ server/api/v2/[...path].ts
@@ -5,9 +5,13 @@ export default defineEventHandler(async (event) => {
   const queryParam = getQuery(event)
   const path = getRouterParam(event, 'path') ?? ''
 
+  const customQuery = {
+    ...queryParam,
+    per_page: 3
+  }
   if (apiAcessToken) {
     event.node.req.headers.authorization = `Bearer ${apiAcessToken}`
   }
 
-  return await proxyRequest(event, withQuery(joinURL(apiBaseUrl, '/public/v2', path), queryParam))
+  return await proxyRequest(event, withQuery(joinURL(apiBaseUrl, '/public/v2', path), customQuery))
 })
